3df828fa2c7ca01692119990738394d17a325129
Chase Sillevis <chase@sillevis.net>
2015-03-01 18:14:15 +0100

    Fix loading config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 // Require given configuration
-if (fs.existsSync('./config/data.js')) {
+if (fs.existsSync(__dirname + '/config/data.js')) {
   exports = module.exports = require('./config/data.js');
 }
 else {
