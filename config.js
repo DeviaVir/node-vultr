117db175f69a3d560f9e747a7ecaded86b5b5ddd
Chase <chase@sillevis.net>
2016-02-08 15:45:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 // Require given configuration
 if (fs.existsSync(__dirname + '/config/data.js')) {
-  exports = module.exports = require('./config/data.js');
+  exports = module.exports = require(__dirname + '/config/data.js');
 }
 else {
   exports = module.exports = {};
